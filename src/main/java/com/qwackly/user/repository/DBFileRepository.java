76662fe19c67478fdba7d60528dbbdbd1492ffb4
abhinav.k <abhinav.k@endurance.com>
2020-07-26 01:18:32 +0530

    delete unused VideController, DBFileEntity etc

--- src/main/java/com/qwackly/user/repository/DBFileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.qwackly.user.repository;
-
-import com.qwackly.user.model.DBFileEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DBFileRepository extends JpaRepository<DBFileEntity, String> {
-}
